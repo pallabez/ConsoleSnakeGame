5d9b00d064beb549e65f413d1cba841ae10dec1d
Pallab Sonowal <60305729+PallabSonowal@users.noreply.github.com>
2020-04-07 23:34:42 +0530

    Commented out the source code

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
+#include <conio.h>                  //For giving input without pressing enter.
+#include <windows.h>                //For screen clear function
 
 bool gameOver, stop;
 const int width = 20;
@@ -15,37 +15,37 @@ eDirection dir;
 void Setup() {
     nTail = 0;
     gameOver = false;
-    dir = STOP; 
-    x = width / 2;
-    y = height / 2;
-    fruitX = (rand() % width) + 1;
+    dir = STOP;                     //Initializes the to be in stopping position
+    x = width / 2;                  //Sets the starting point at middle of the map
+    y = height / 2;                 //Same as above
+    fruitX = (rand() % width) + 1;  //+1 so that the fruit doesn't spawn in the border 
     fruitY = (rand() % height) + 1;
     score = 0;
 }
 
 void Draw() {
-    system("cls");
-    for(int i = 0; i <= height + 2; i++) {
+    system("cls");                                          //Clears the screen
+    for(int i = 0; i <= height + 2; i++) {                 
         for(int j = 0; j <= width + 2; j++) {
-            if(i == 0 || i == height + 2) {
+            if(i == 0 || i == height + 2) {                 //Prints out the horizontal border
                 std::cout << "#";
             } else {
-                if(j == 0 || j == width + 2) {
+                if(j == 0 || j == width + 2) {              //Prints out the verticle border
                     std::cout << "|";
-                } else if(i == y && j == x) {
+                } else if(i == y && j == x) {               //Prints out the head of snake
                     std::cout << "O";
-                } else if(i == fruitY && j == fruitX) {
+                } else if(i == fruitY && j == fruitX) {     //Prints out the fruit
                     std::cout << "F";
                 } else {
-                    bool print = false;
+                    bool print = false;                     
                     for(int k = 0; k < nTail; k++) {
-                        if(tailX[k] == j && tailY[k] == i) {
+                        if(tailX[k] == j && tailY[k] == i) {    //Prints out the body
                             std::cout << "*";
-                            print = true;
+                            print = true;                   //Checks if segment of the body is there
                         }
                     }
-                    if(!print) std::cout << " ";
-                }
+                    if(!print) std::cout << " ";            //Reduces a space to maintain border deformity 
+                }                                           //if a body is taking up some space
             }
         }
         std::cout << "\n";
@@ -53,7 +53,7 @@ void Draw() {
     std::cout << "Score: " << score << "\n" << "Made by Pallab XD" << "\n";
 }
 
-void Input() {
+void Input() {                         //Control system
     if (_kbhit()) {
         switch(_getch()) {
         case 'a':
@@ -72,7 +72,7 @@ void Input() {
             stop = true;
             break;
         case 'r':
-            gameOver = false;
+            gameOver = false;           //Retry button
             break;
         }
     }
@@ -109,18 +109,18 @@ void Logic() {
         default:
             break;
     }
-    if(x >= width + 2 || x <= 0 || y >= height + 2 || y <= 0) {
+    if(x >= width + 2 || x <= 0 || y >= height + 2 || y <= 0) {     //Gameover if snake touches the boundary
         gameOver = true;
     }
     for(int i = 0; i < nTail; i++) {
-        if(x == tailX[i] && y == tailY[i]) {
+        if(x == tailX[i] && y == tailY[i]) {                        //Gameover if snake head touches the tail
             gameOver = true;
         }
     }
     
-    if(x == fruitX && y == fruitY) {
+    if(x == fruitX && y == fruitY) {                                //Eats fruit
         score += 10;
-        fruitX = (rand() % width) + 1;
+        fruitX = (rand() % width) + 1;                              //Randomly generates another fruit after eating the first one
         fruitY = (rand() % height) + 1;
         nTail++;
     }
